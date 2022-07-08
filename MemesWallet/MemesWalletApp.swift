bdcc1fea64c93fb392d8f4015907c6ee9cef64af
Spyrosskordos <spyrosskordos@gmail.com>
2022-07-08 19:29:34 +0300

    Initial Commit

---
+++ MemesWallet/MemesWalletApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemesWalletApp.swift
+//  MemesWallet
+//
+//  Created by Sko on 8/7/22.
+//
+
+import SwiftUI
+
+@main
+struct MemesWalletApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
